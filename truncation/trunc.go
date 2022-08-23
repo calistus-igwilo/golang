8156431fe4e616b77076d66a40f6a935281538d1
calistus-igwilo <ebunilo@yahoo.com>
2022-08-23 18:14:31 +0100

    truncate a float to integer

--- truncation/trunc.go
+++ truncation/trunc.go
@@ -1,3 +1,8 @@
+/*
+A program which prompts the user to enter a floating point number and prints the
+integer which is a truncated version of the floating point number that was entered
+*/
+
 package main
 
 import "fmt"
@@ -6,6 +11,6 @@ var floatNum float64
 func main(){
 	fmt.Println("Enter a float value : ")
 	fmt.Scanf("%f", &floatNum)
-	fmt.Printf("%.0f", floatNum)
+	fmt.Printf("Truncated float: %.0f ", floatNum)
 
 }
