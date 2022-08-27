1928ff20f826450f9dd4295a31d1877180fc6a22
calistus-igwilo <ebunilo@yahoo.com>
2022-08-27 11:06:39 +0100

    bubble sort

---
+++ read/bubble_sort/bubble_sort.go
@@ -0,0 +1,26 @@
+/* 
+Write a Bubble Sort program in Go. The program should prompt the user 
+to type in a sequence of up to 10 integers. The program should print 
+the integers out on one line, in sorted order, from least to greatest. 
+
+As part of this program, you should write a function called BubbleSort() 
+which takes a slice of integers as an argument and returns nothing. 
+The BubbleSort() function should modify the slice so that the elements 
+are in sorted order.
+
+A recurring operation in the bubble sort algorithm is the Swap operation 
+which swaps the position of two adjacent elements in the slice. You 
+should write a Swap() function which performs this operation. Your 
+Swap() function should take two arguments, a slice of integers and 
+an index value i which indicates a position in the slice. The Swap() 
+function should return nothing, but it should swap the contents of 
+the slice in position i with the contents in position i+1.
+*/
+
+package main 
+
+import "fmt"
+
+func main() {
+	
+}
---
+++ review/review.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	var x int
+	var y *int
+	z := 3
+	y = &z
+	x = &y
+  }
